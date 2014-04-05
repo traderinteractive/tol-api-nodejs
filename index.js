df1780f46536dec4c4ca66df0601c580b7f03951
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2014-04-05 12:17:42 -0400

    Make version part of the base url, like our other API clients.
    This will break backwards compatibility.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var api = require('./lib/api');
 var _ = require('underscore');
 
-var createClient = exports = module.exports = function(apiUrl, apiVersion, clientId, clientSecret) {
+var createClient = exports = module.exports = function(apiUrl, clientId, clientSecret) {
   var client = _.extend({}, api);
   client.init();
   client.set('url', apiUrl);
-  client.set('version', apiVersion);
   client.set('clientId', clientId);
   client.set('clientSecret', clientSecret);
 
