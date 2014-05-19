3db57d5cd15e5faf55598dbe9135e9899cb75339
Robert Bittle <guywithnose@gmail.com>
2014-05-19 16:04:26 -0400

    Add user tokens

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var api = require('./lib/api');
 var _ = require('underscore');
 
-var createClient = exports = module.exports = function(apiUrl, clientId, clientSecret) {
+var createClient = function(apiUrl, clientId, clientSecret) {
   var client = _.extend({}, api);
   client.init();
   client.set('url', apiUrl);
-  client.set('clientId', clientId);
-  client.set('clientSecret', clientSecret);
+
+  if (_.isFunction(clientId)) {
+    client.set('tokenFetcher', clientId);
+  } else {
+    client.set('tokenFetcher', _.partial(api.getClientCredentialsToken, apiUrl + '/token', clientId, clientSecret));
+  }
 
   return client;
+};
+
+module.exports = exports = {
+  createClient: createClient,
+  getClientCredentialsToken: api.getClientCredentialsToken,
+  getResourceOwnerPasswordCredentialsToken: api.getResourceOwnerPasswordCredentialsToken
 };
